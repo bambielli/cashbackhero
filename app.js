2d5b5aab19d9de4eac56f674dbc0db0ce9857642
Brian Ambielli <brian.ambielli@gmail.com>
2017-03-12 20:09:47 -0500

    renamed migrations to have consistent naming scheme (using dashes) and created profiles migration

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
 const logger = require('morgan')
-const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const passport = require('passport')
@@ -33,10 +32,11 @@ if (app.get('env') === 'production') {
   app.set('trust proxy', 1) // trust first proxy
   sess.cookie.secure = true // serve secure cookies
 }
+console.log('env is', app.get('env'))
 
 app.use(session(sess))
 
-//must be done AFTER session setup
+// must be done AFTER session setup
 app.use(passport.initialize())
 app.use(passport.session())
 require('./passport.config')(passport)
@@ -46,11 +46,10 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(require('less-middleware')(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -60,14 +59,14 @@ app.use('/api/cards', cardRoutes)
 app.use('/', appRoutes)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found')
   err.status = 404
   next(err)
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
@@ -75,6 +74,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500)
   res.render('error')
-});
+})
 
 module.exports = app
