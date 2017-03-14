5c269543d33af1b1348c3d41b680ea8efa4d98ad
Brian Ambielli <brian.ambielli@gmail.com>
2017-03-13 20:29:50 -0500

    renaming cb

--- passport.config.js
+++ passport.config.js
@@ -1,34 +1,33 @@
 const FacebookStrategy = require('passport-facebook')
 const { users } = require('./sql')
 
-const serializeUser = (user, done) => {
-  done(null, user.id) //this only needs the user.id. the full user object is retrieved via deserialize user.
+const serializeUser = (user, cb) => {
+  cb(null, user.id) // this only needs the user.id. the full user object is retrieved via deserialize user.
 }
 
-const deserializeUser = (id, done) => {
+const deserializeUser = (id, cb) => {
   users.getUser(id)
     .then((data) => {
-      console.log(data)
-      done(null, data)
+      cb(null, data)
     })
     .catch((err) => {
-      done(err, null)
+      cb(err, null)
     })
 }
 
 facebookStrategy = new FacebookStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: process.env.BASE_URL + '/auth/facebook/callback',
-    profileFields: ['id', 'name', 'gender', 'picture', 'email', 'age_range', 'locale']
-  },
-  function (accessToken, refreshToken, profile, done) {
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.BASE_URL + '/auth/facebook/callback',
+  profileFields: ['id', 'name', 'gender', 'picture', 'email', 'age_range', 'locale']
+},
+  function (accessToken, refreshToken, profile, cb) {
     users.getOrCreateUser(profile._json)
       .then((data) => {
-        done(null, data)
+        cb(null, data)
       })
       .catch((err) => {
-        done(err, null)
+        cb(err, null)
       })
   }
 )
