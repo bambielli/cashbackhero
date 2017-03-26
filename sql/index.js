bf30a45f9f0bbd92e921af3039c34b13dbf3a6ab
Brian Ambielli <brian.ambielli@gmail.com>
2017-03-25 21:23:10 -0500

    need to add uniqueness constraint to this migration, so the getOrCreateEmptyWallet sql statement will work

--- sql/index.js
+++ sql/index.js
@@ -20,17 +20,6 @@ module.exports = {
     }
   },
   users: {
-    createUser: (data) => {
-      const facebook_id = parseInt(data.id)
-      const age_range_max = data.age_range.max || null
-      const age_range_min = data.age_range.min || null
-      const userData = Object.assign({}, data, {
-        facebook_id,
-        age_range_max,
-        age_range_min
-      })
-      return db.one(users.createUser, userData)
-    }
     getOrCreateUser: (data) => {
       const facebook_id = parseInt(data.id)
       const age_range_max = data.age_range.max || null
