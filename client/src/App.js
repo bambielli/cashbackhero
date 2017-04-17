6cba6bb54278da6b7a7adfa74bf22fe3989d0488
Brian Ambielli <brian.ambielli@gmail.com>
2017-04-16 22:15:39 -0500

    making a fetch to the api server and proxying through webpack dev server. cool beans

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      'cards': []
+    }
+  }
+  componentDidMount() {
+    console.log('mounted component')
+    fetch('/api/cards', {
+      accept: 'application/json'
+    }).then((res)=>{
+        if (res.ok) {
+          return res
+        } else {
+          throw Error('cards fetch threw an error')
+        }
+    }).then((res) => {
+        return res.json()
+    }).then((data) => {
+        console.log(data)
+        this.setState({'cards': data.data})
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +36,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {this.state.cards.map((card)=>
+            <li key={card.id}>{card.name}</li>
+          )}
+        </ul>
       </div>
     );
   }
