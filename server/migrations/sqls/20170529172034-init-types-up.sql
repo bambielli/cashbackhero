179e5f3c9d009729b4989d0088c4057ff81d36e9
Brian Ambielli <brian.ambielli@gmail.com>
2017-05-29 21:15:23 -0500

    types locations and location_types tables. seed data for all

---
+++ server/migrations/sqls/20170529172034-init-types-up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE types (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR
+);
+
+INSERT INTO types (name)
+VALUES ('Home Improvement Stores'),
+('Costco'),
+('Sams Club'),
+('Wholesale Clubs'),
+('Restaurants'),
+('Amazon'),
+('Grocery Stores'),
+('Drug Stores'),
+('TJ Maxx'),
+('Travel'),
+('Gas Stations');
---
+++ server/migrations/sqls/20170529172344-init-locations-up.sql
@@ -0,0 +1,31 @@
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR
+);
+
+INSERT INTO locations (name)
+VALUES
+('Amazon'),
+('TJ Maxx'),
+('Marianos Fresh Market'),
+('Jewel Osco'),
+('Whole Foods'),
+('Costco'),
+('Sams Club'),
+('Shell'),
+('BP'),
+('Chevron'),
+('Home Depot'),
+('Lowes'),
+('Au Cheval'),
+('Hopleaf'),
+('Big Star'),
+('CVS'),
+('Wallgreens'),
+('Expedia'),
+('Orbitz'),
+('Alamo'),
+('Hertz'),
+('Chicago Transit Authority'),
+('Uber'),
+('Lyft');
