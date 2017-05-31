4a60fedd424e93d25cf70e0c103008e32afbb57b
Brian Ambielli <brian.ambielli@gmail.com>
2017-05-30 23:20:23 -0500

    adding start and end date columns to cashback table, and changing ERD to reflect new state of the world

--- server/migrations/sqls/20170531022820-init-cashback-up.sql
+++ server/migrations/sqls/20170531022820-init-cashback-up.sql
@@ -3,11 +3,13 @@ CREATE TABLE cashback (
     card_id INTEGER REFERENCES cards (id),
     type_id INTEGER REFERENCES types (id),
     amount NUMERIC(5, 2),
+    start_date DATE,
+    end_date DATE,
     UNIQUE (card_id, type_id)
 );
 
-INSERT INTO cashback (card_id, type_id, amount)
+INSERT INTO cashback (card_id, type_id, amount, start_date, end_date)
 VALUES
-(6, 6, 5.0),
-(6, 5, 2.0),
-(6, 11, 2.0)
+(6, 6, 5.0, '2017-05-01', '210-12-31'),
+(6, 5, 2.0, '2017-05-01', '210-12-31'),
+(6, 11, 2.0, '2017-05-01', '210-12-31')
