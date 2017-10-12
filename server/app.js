58458dc87a607833daa09ab2138ee583919bde3a
Brian Ambielli <brian.ambielli@gmail.com>
2017-10-11 21:08:35 -0500

    didnt really work need to think about this again

--- server/app.js
+++ server/app.js
@@ -45,10 +45,6 @@ app.use(passport.initialize())
 app.use(passport.session())
 require('./passport.config')(passport)
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
@@ -56,14 +52,25 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: false
 }))
-app.use(require('less-middleware')(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/static', express.static(path.join(__dirname, '../client/build')))
+app.use('/static', express.static(path.join(__dirname, '../client/build/static')))
 
 app.use('/api', isLoggedIn)
 app.use('/auth/facebook', facebookRoutes)
 app.use('/api/cards', cardRoutes)
 app.use('/api/users', userRoutes)
 app.use('/', appRoutes)
+
+// Catchall route to serve index page or redirect to login
+app.get('*', (req, res) => {
+  if (app.get('env') === 'production') {
+    res.redirect('/static/index.html')
+  } else {
+    res.redirect('/static/index.html')
+  }
+})
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -78,9 +85,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
   res.status(err.status || 500)
-  res.render('error')
 })
 
 module.exports = app
--- server/routes/app/appRoutes.js
+++ server/routes/app/appRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { users } = require('../../sql')
+const path = require('path')
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'Cash Back Hero'})
+  res.render('landing', {title: 'Cash Back Hero'})
 })
 
 router.get('/login', (req, res) => {
@@ -12,24 +12,6 @@ router.get('/login', (req, res) => {
 
 router.get('/badAuth', (req, res) => {
   res.send('bad authentication')
-})
-
-router.get('/home', (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    res.redirect('/login')
-  } else {
-    users.getUserCards(req.user.id)
-      .then((data) => {
-        if (data.length === 0) {
-          cards = 'No cards in wallet yet'
-        }
-        // the below is broken. it returns [object Object] instead of parsing
-        res.render('home', {cards: data})
-      })
-      .error(function (err) {
-        res.send(err.message)
-      })
-  }
 })
 
 module.exports = router
