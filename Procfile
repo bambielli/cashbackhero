d471728d0b4dd7a4c2d9dec16055614ee4bc6406
Brian Ambielli <brian.ambielli@gmail.com>
2017-10-13 09:12:54 -0500

    adding to procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: cd client && yarn run build && mv build/ ../server/build/ && cd ../server && db-migrate up && cd .. && yarn start
+web: ./build.sh
--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
+# Meant to be run on heroku via procfile. It deletes the client code after it is built, to save memory on dyno
+# In the future, maybe we build a docker container during build step
 cd client &&
+yarn install &&
 yarn run build &&
 mv build/ ../server/build/ &&
 cd ../server &&
+rm -rf client &&
 db-migrate up &&
 cd .. &&
 yarn start
