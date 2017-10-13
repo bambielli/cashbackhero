6f9f2382b3a8bd3c208459323c44931335313e23
Brian Ambielli <brian.ambielli@gmail.com>
2017-10-13 09:59:03 -0500

    adding some texts to the postbuild script so we know what is going on

--- postbuild.sh
+++ postbuild.sh
@@ -1,6 +1,8 @@
 # This is run on heroku after the server has been built
 cd client &&
+echo "Installing all dependencies for React App (including dev dependencies)" &&
 yarn install --production=false && # installing client deps in the client folder
 yarn run build &&
 mv build/ ../build/ &&
+echo "removing node modules" &&
 rm -rf node_modules
