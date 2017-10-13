f957a1b151655fad378b4d1066d90572d874aeee
Brian Ambielli <brian.ambielli@gmail.com>
2017-10-13 09:48:16 -0500

    fixing postbuild

--- postbuild.sh
+++ postbuild.sh
@@ -1,4 +1,5 @@
+# This is run on heroku after the server has been built
 cd client &&
 yarn install && # installing client deps in the client folder
 yarn run build &&
-mv build/ ../build/ &&
+mv build/ ../build/
