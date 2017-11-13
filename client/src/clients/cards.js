63389e979ec910ed9c3a24e0f8af5abe080d4928
Brian Ambielli <brian.ambielli@gmail.com>
2017-11-12 22:25:08 -0600

    redirect in prod

--- client/src/clients/cards.js
+++ client/src/clients/cards.js
@@ -2,13 +2,15 @@ function getCards () {
     return fetch('/api/cards', {
         credentials: "include"
     })
-        .then(res => {
-            if (res.ok) {
-                return res.json()
-            } else {
-                return Promise.reject('Internal Server Error')
-            }
-        })
+    .then(res => {
+        if (res.ok) {
+            return res.json()
+        } else if (process.env.NODE_ENV === 'development') {
+            return Promise.reject('Internal Server Error')
+        } else {
+            window.location.replace('/login')
+        }
+    })
 }
 
 module.exports = {
