e75f0ea6c66cae6df5e3bef4ab513134233963ac
Brian Ambielli <brian.ambielli@gmail.com>
2017-11-18 13:07:46 -0600

    CSS modules

--- client/src/views/LandingPage.js
+++ client/src/views/LandingPage.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
-import '../App.css';
+import styles from '../App.css';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class LandingPage extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.app}>
+        <div className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
           <h2>Welcome to Cashbackhero</h2>
         </div>
         <RaisedButton label="Login" href="/login" />
